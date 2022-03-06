d990c270e26f59be9695d5582ff10d939e2d12c8
James T. Sprinkle <the-grue@hotmail.com>
2022-03-05 19:19:52 -0800

    Converted "Xor" to "^" and commented out macro. Build checksum is equivalent after change.

--- linux/asmsubs.c
+++ linux/asmsubs.c
@@ -314,14 +314,14 @@ byte                                   temp[33];
  while ( len-- > 0
   ) {
    c = (Bit_16(*beg_str++) & 0xFF) | 0x20;
-   *starting_block = c Xor ((*starting_block ShiftedLeft   2)  |
+   *starting_block = c ^ ((*starting_block ShiftedLeft   2)  |
                             (*starting_block ShiftedRight 14));
-   *delta_entry    = c Xor ((*delta_entry    ShiftedRight  2)  |
+   *delta_entry    = c ^ ((*delta_entry    ShiftedRight  2)  |
                             (*delta_entry    ShiftedLeft  14));
    c = (Bit_16(*end_str--) & 0xFF) | 0x20;
-   *delta_block    = c Xor ((*delta_block    ShiftedLeft   2)  |
+   *delta_block    = c ^ ((*delta_block    ShiftedLeft   2)  |
                             (*delta_block    ShiftedRight 14));
-   *starting_entry = c Xor ((*starting_entry ShiftedRight  2)  |
+   *starting_entry = c ^ ((*starting_entry ShiftedRight  2)  |
                             (*starting_entry ShiftedLeft  14));
   }; */
  asm            xor     ax,ax
--- linux/langext.h
+++ linux/langext.h
@@ -63,8 +63,8 @@ ListTypeOf(Generic_Element);
 
 /* Bitwise operators */
 /*#define And                  &
-#define Or                   |*/
-#define Xor                  ^
+#define Or                   |
+#define Xor                  ^*/
 #define Complement           ~
 /* Some other operators */
 #define Addr(exp)            &(exp)
